1f9d5df20c4a9da57c006e51ce36a6582b3196a1
Your Name <your.email@example.com>
2026-06-25 13:22:35 +0530

    Design: Correctly apply SVG rounded corners filter on parent wrapper of Contact clip paths

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,8 +10,10 @@ interface ImageClipBoxProps {
 }
 
 const ImageClipBox = ({ src, clipClass, alt }: ImageClipBoxProps) => (
-  <div className={clipClass}>
-    <img src={src} alt={alt || "XMO platform feature"} />
+  <div style={{ filter: "url(#flt_tag)" }} className="w-full h-full">
+    <div className={clipClass}>
+      <img src={src} alt={alt || "XMO platform feature"} />
+    </div>
   </div>
 );
 
@@ -55,6 +57,33 @@ const Contact = () => {
           </a>
         </div>
       </div>
+
+      {/* SVG filter definition for rounded clip-paths */}
+      <svg
+        className="invisible absolute size-0"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <defs>
+          <filter id="flt_tag">
+            <feGaussianBlur
+              in="SourceGraphic"
+              stdDeviation="8"
+              result="blur"
+            />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+              result="flt_tag"
+            />
+            <feComposite
+              in="SourceGraphic"
+              in2="flt_tag"
+              operator="atop"
+            />
+          </filter>
+        </defs>
+      </svg>
     </div>
   );
 };
